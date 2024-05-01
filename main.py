3546b648978c799d4cd8c3d9f49e84d509dbc9c2
Frederick Morrison <frederick.morrison@outlook.com>
2024-05-01 15:20:04 -0600

    add PyMongo connection defs

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import sys
+import os,sys
+from dotenv import load_dotenv
+from pymongo import MongoClient
 
+
+def get_connection_string() -> str:
+    load_dotenv()
+
+    template: str = os.environ.get('mongodb_connection_template')
+    uid: str = os.environ.get('mongodb_uid')
+    pwd: str = os.environ.get('mongodb_pwd')
+
+    return f'mongodb+srv://{uid}:{pwd}@{template}'
+
+def get_mongodb_client() -> MongoClient:
+    #print(f'{get_connection_string()=}')
+    return MongoClient(get_connection_string())
 
 def get_python_version() -> str:
     return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
@@ -7,3 +22,4 @@ def get_python_version() -> str:
 
 if __name__ == '__main__':
     print(f"Python version: {get_python_version()}")
+
