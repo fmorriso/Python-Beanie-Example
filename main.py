690875e971646b5b12d69b2b939956e2d87b5b5c
Fred Morrison <frederick.morrison@outlook.com>
2025-04-22 12:54:21 -0600

    add get_current_datetime method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 from importlib.metadata import version
 
@@ -6,6 +7,26 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from models import Products
 from program_settings import ProgramSettings
+from datetime import datetime, timezone
+
+
+def get_python_version() -> str:
+    return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+
+
+def get_package_version(package_name: str) -> str:
+    return version(package_name)
+
+def get_current_datetime() -> str:
+    # Get the current UTC time
+    current_gmt = datetime.now(timezone.utc)
+
+    # Format the output to include fractional seconds to 2 decimal places
+    formatted_gmt = f"{current_gmt.year}-{current_gmt.month:02d}-{current_gmt.day:02d} " \
+                    f"{current_gmt.hour:02d}:{current_gmt.minute:02d}:{current_gmt.second:02d}." \
+                    f"{current_gmt.microsecond // 10000:02d}"
+
+    return formatted_gmt
 
 
 async def read_products():
@@ -34,26 +55,22 @@ async def init():
     client = AsyncIOMotorClient("mongodb://user:pass@host:27017")
 
     # Init beanie with the Product document class
+    print(f'before init_beanie: {get_current_datetime()}')
     await init_beanie(database = client.db_name, document_models = [Products])
+    print(f'after  init_beanie: {get_current_datetime()}')
 
-
-def get_python_version() -> str:
-    return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-
-
-def get_package_version(package_name: str) -> str:
-    return version(package_name)
 
 async def main():
     await init()
+
 
 if __name__ == '__main__':
     print(f"Python version: {get_python_version()}")
     print(f"Beanie version: {get_package_version('beanie')}")
     print(f"Motor version: {get_package_version('motor')}")
     print(f"Pydantic version: {get_package_version('pydantic')}")
-    main()
+    asyncio.run(main())
 
     # asyncio.run(init())
-    #display_collections()
+    # display_collections()
     # asyncio.run(read_products())
